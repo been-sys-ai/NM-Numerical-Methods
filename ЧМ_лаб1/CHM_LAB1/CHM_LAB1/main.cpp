28785dbb2a15ad318160ae47ab0c5f6791dbc3e4
been-sys <volhinbogdan1@gmail.com.com>
2026-09-21 14:41:31 +0700

    First commit

---
+++ ЧМ_лаб1/CHM_LAB1/CHM_LAB1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CHM_LAB1
+//
+//  Created by bogdan volkhin on 9/21/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
